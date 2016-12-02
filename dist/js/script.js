9652c3b2e1ba184d267488956767f2e1db1f1613
Ása Einarsdóttir <alsaugran@gmail.com>
2016-12-02 11:17:16 +0000

    animation on frontage works.

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,6 +1,20 @@
 $(document).ready(function() {
+
+	// toggle hamburger menu
 	$('.hamburger-menu').click(function() {
 		$('.bar').toggleClass('animate');
-    $('.top-nav').toggleClass('visible');
+		$('.nav').toggleClass( 'display' );
 	});
+
+
+	// Hide div on Scroll
+	$(window).scroll(function() {
+
+	 	if ($(this).scrollTop()>0) {
+			$('.name').fadeOut() && $('.bottom').fadeOut();
+		} else {
+		 	$('.name').fadeIn() && $('.bottom').fadeIn();
+		}
+	});
+
 });
