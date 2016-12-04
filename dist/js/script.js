ff43272ff97a95b7be02ca3230bb2fe331f7d82e
Ása Einarsdóttir <alsaugran@gmail.com>
2016-12-04 19:52:02 +0000

    Trying to animate one shape into another shape.

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,20 +1,62 @@
 $(document).ready(function() {
 
+	// To delay page load with PreLoader
+	// setTimeout(function(){
+	// 	$('#loader').fadeOut('slow', function() {
+	// 	$('#overlay').fadeOut('slow');
+	// }, 3000);
+
 	// toggle hamburger menu
 	$('.hamburger-menu').click(function() {
 		$('.bar').toggleClass('animate');
 		$('.nav').toggleClass( 'display' );
 	});
 
-
 	// Hide div on Scroll
 	$(window).scroll(function() {
 
 	 	if ($(this).scrollTop()>0) {
-			$('.name').fadeOut() && $('.bottom').fadeOut();
+			$('.bottom').fadeOut();
 		} else {
-		 	$('.name').fadeIn() && $('.bottom').fadeIn();
+		 	$('.bottom').fadeIn();
 		}
+
+		// Nav background turns white on scroll
+		if ($(this).scrollTop()>400) {
+			$('.header-bg').fadeIn();
+		} else {
+		 	$('.header-bg').fadeOut();
+		}
+
+		// Left sidebar scroll and then fixed
+		// if ( $(this).scrollTop() >= $(window).height() ){
+		// menu.removeClass('case-study-1').addClass('fixed-sidebar');
+		// } else {
+		// menu.removeClass('fixed-sidebar').addClass('case-study-1');
+		// }
+
+	});
+
+
+
+	// Left nav scroll to content
+
+	$("#chapter-1-button").click(function() {
+		$('html, body').animate({
+				scrollTop: $("#chapter-1").offset().top
+		}, 2000);
+	});
+
+	$("#chapter-2-button").click(function() {
+		$('html, body').animate({
+				scrollTop: $("#chapter-2").offset().top
+		}, 2000);
+	});
+
+	$("#chapter-3-button").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#chapter-3").offset().top
+    }, 2000);
 	});
 
 });
